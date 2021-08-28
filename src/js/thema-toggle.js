065a1ad2ce00263e02355f81c05d0927cfc0cdc9
maicaosgz <malconaugusto@outlook.com>
2021-08-27 23:09:15 -0300

    Melhorias para o thema.

--- src/js/thema-toggle.js
+++ src/js/thema-toggle.js
@@ -1,19 +1,29 @@
+
 const toggleTheme = document.querySelector("#toggleButton");
 let body = document.querySelector('body');
 let allH2 = document.querySelectorAll('h2');
 let allBtn = document.querySelectorAll('button');
-let imgBgHome = document.querySelector('#bg');
+let imgCartoonHome = document.querySelector('#bg');
+let imgLogo = document.querySelector('header a ');
+let btnSecond = document.querySelector('a.button.outlined');
+let separator = document.querySelectorAll('.separator div');
 
 toggleTheme.addEventListener('click',()=>{
-    body.classList.toggle('linear-dark');
-    if(body.classList.contains('linear-dark')){
+    body.classList.toggle('bg-dark');
+    if(body.classList.contains('bg-dark')){
         allH2.forEach((item)=>{
             item.classList.add('white');
         });
         allBtn.forEach((item)=>{
             item.classList.add('blue');
         });
-        imgBgHome.style.background = "url(/images/home-bg-img-dark.svg) top/contain no-repeat";
+        imgCartoonHome.style.background = "url(/images/home-bg-img-dark.svg) top/contain no-repeat";
+        imgLogo.innerHTML = '<img id="logo" src="/images/logo-dark.svg" alt="Rocket.Q Logo" />';
+        btnSecond.innerHTML= '<img src="images/users-dark.svg" alt="Criar Sala"> Criar Sala';
+        btnSecond.classList.add('gradient-btn');
+        separator.forEach((item)=>{
+            item.classList.add('white');
+        });
     }else{
         allH2.forEach((item)=>{
             item.classList.remove('white');
@@ -21,7 +31,12 @@ toggleTheme.addEventListener('click',()=>{
         allBtn.forEach((item)=>{
             item.classList.remove('blue');
         });
-        imgBgHome.style.background = "url(/images/home-bg-img.svg) top/contain no-repeat";
+        imgCartoonHome.style.background = "url(/images/home-bg-img.svg) top/contain no-repeat";
+        imgLogo.innerHTML = '<img id="logo" src="/images/logo.svg" alt="Rocket.Q Logo" />';
+        btnSecond.innerHTML= '<img src="images/users.svg" alt="Criar Sala"> Criar Sala';
+        btnSecond.classList.remove('gradient-btn');  
+        separator.forEach((item)=>{
+            item.classList.remove('white');
+        });
     }
 });
-
